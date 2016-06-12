22733d753e20660847ce224f1d1c97768aa339ea
Johann Ott <johann.ott@gmail.com>
2016-06-12 17:58:49 +0100

    First Commit - Header, Footer, Pages and Basic Controllers

---
+++ app.js
@@ -0,0 +1,55 @@
+// MODULE
+var portfolioApp = angular.module('portfolioApp', ['ngRoute', 'ngResource', 'ngAnimate']);
+
+// ROUTES
+portfolioApp.config(function ($routeProvider) {
+
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/home.htm',
+        controller: 'homeController'
+    })
+
+    .when('/skills', {
+        templateUrl: 'pages/skills.htm',
+        controller: 'skillsController'
+    })
+
+    .when('/projects', {
+        templateUrl: 'pages/projects.htm',
+        controller: 'projectsController'
+    })
+
+    .when('/interests', {
+        templateUrl: 'pages/interests.htm',
+        controller: 'interestsController'
+    })
+
+    .when('/contact', {
+        templateUrl: 'pages/contact.htm',
+        controller: 'contactController'
+    })
+
+});
+
+// CONTROLLERS
+portfolioApp.controller('homeController', ['$scope', function($scope) {
+  $scope.pageClass = 'page-home';
+}]);
+
+portfolioApp.controller('skillsController', ['$scope', function($scope) {
+  $scope.pageClass = 'page-skills';
+}]);
+
+portfolioApp.controller('projectsController', ['$scope', function($scope) {
+  $scope.pageClass = 'page-projects';
+}]);
+
+portfolioApp.controller('interestsController', ['$scope', function($scope) {
+  $scope.pageClass = 'page-interests';
+}]);
+
+portfolioApp.controller('contactController', ['$scope', function($scope) {
+  $scope.pageClass = 'page-contact';
+}]);
